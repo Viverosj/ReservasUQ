fb6c2d618baefe51d9a981ed1d28608a639432bb
Viverosj <141973610+Viverosj@users.noreply.github.com>
2024-10-24 20:54:14 -0500

    .

--- src/main/java/co/edu/uniquindio/reservasuq/App/ReservasUQApp.java
+++ src/main/java/co/edu/uniquindio/reservasuq/App/ReservasUQApp.java
@@ -1,4 +1,4 @@
-package co.edu.uniquindio.reservasuq;
+package co.edu.uniquindio.reservasuq.App;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ReservasUQApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ReservasUQApp.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Reservas");
         stage.setScene(scene);
         stage.show();
     }
