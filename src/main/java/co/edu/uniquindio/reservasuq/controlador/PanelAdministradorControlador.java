dcc6ceafe906033d521fc5ee1b0bc2c5ffeccd91
AngelicaReyesB <angelicam.reyesb@uqvirtual.edu.co>
2024-11-05 12:27:30 -0500

    .

--- src/main/java/co/edu/uniquindio/reservasuq/controlador/PanelAdministradorControlador.java
+++ src/main/java/co/edu/uniquindio/reservasuq/controlador/PanelAdministradorControlador.java
@@ -1,13 +1,46 @@
 package co.edu.uniquindio.reservasuq.controlador;
 
 import co.edu.uniquindio.reservasuq.controlador.observador.Observable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PanelAdministradorControlador implements Observable, Initializable {
 
+    @FXML private TableColumn<?, ?> ColHorario;
+    @FXML private TableColumn<?, ?> colAforo;
+    @FXML private TableColumn<?, ?> colCosto;
+    @FXML private TableColumn<?, ?> colIdInstalacion;
+    @FXML private TableColumn<?, ?> colTipoInstalacion;
+
+    @FXML
+    void agregarInstalacion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cbHorario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cbTipoInstalacion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAforo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtIdInstalacion(ActionEvent event) {
+
+    }
     private Observable observable;
 
     @Override
